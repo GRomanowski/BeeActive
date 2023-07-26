09c4ee5e930701f44b2a831ac1ceb1705b294b38
gromanow <grzegorz.romanowski@capgemini.com>
2023-07-26 14:56:50 +0200

    Adding homework 5 solution

---
+++ src/User.java
@@ -0,0 +1,16 @@
+package src;
+
+public class User {
+    public User() {
+        System.out.println("konstruktor");
+    }
+
+    public User(String user, String pass) {
+
+        System.out.println("Welcome");
+    }
+
+    public void Login(String a, String b) {
+
+    }
+}
---
+++ src/UserTest.java
@@ -0,0 +1,9 @@
+package src;
+
+public class UserTest {
+    public static void main(String[] args) {
+
+        User user = new User();
+        User user2 = new User("adam", "Bartek");
+    }
+}
