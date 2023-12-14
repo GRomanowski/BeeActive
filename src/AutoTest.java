ca51d9d0b983fc8b2a5dbc5589a1f8b9ca1579a5
GRAK <hjobster@gmail.com>
2023-12-14 16:15:59 +0100

    14.12.23

--- src/AutoTest.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class AutoTest {
-    public static void main(String[] args) {
-
-        Auto auto = new Auto("Mercedes", "A", 2018, 20000);
-
-    }
-}
--- src/UserTest.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class UserTest {
-    public static void main(String[] args) {
-
-        User user = new User();
-        User user2 = new User("adam", "Bartek");
-    }
-}
