3b7405a9a8be10a4a204d86a84568fdc2a155a2b
GRAK <hjobster@gmail.com>
2023-12-15 22:36:01 +0100

    14.12.23

---
+++ src/Imperatywy/Zad1_1.java
@@ -0,0 +1,13 @@
+package src.Imperatywy;
+
+import java.util.Scanner;
+
+public class Zad1_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+
+        System.out.println("pole prostokąta = " + (a * b));
+    }
+}
